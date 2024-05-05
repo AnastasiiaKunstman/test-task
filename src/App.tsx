eb9ef842d4cf33e40a24c81749a7256e9ee8b94a
Anastasiia Kunstman <anastasiia.kunstman@yandex.ru>
2024-05-06 00:48:26 +0500

    fix: устранение ошибок сборки

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Details from './components/Details/Details';
 import Widget from './components/Widget/Widget';
-// import Widget from './components/Widget/Widget';
-
-export interface Item {
-  id: number;
-  name: string;
-  text: string;
-  checked: boolean;
-}
 
 const App = () => {
   return (
@@ -19,7 +9,7 @@ const App = () => {
       <Widget />
       <Footer />
     </>
-  );
+  )
 };
 
 export default App
